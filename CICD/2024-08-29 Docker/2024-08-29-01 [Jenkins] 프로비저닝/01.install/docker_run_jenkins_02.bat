566b94abac522e20309ba2923ddff1101ebade9a
changwskr <changwskr@naver.com>
2024-08-31 06:46:06 +0900

    aaa

--- CICD/2024-08-29 Docker/2024-08-29-01 [Jenkins] 프로비저닝/01.install/docker_run_jenkins_02.bat
+++ CICD/2024-08-29 Docker/2024-08-29-01 [Jenkins] 프로비저닝/01.install/docker_run_jenkins_02.bat
@@ -2,7 +2,7 @@ rem "container name은 jenkins-server"
 docker run -d -p 8080:8080 -p 50000:50000 ^
         --name jenkins-server ^
         --restart=on-failure ^
-        -v D:\Work\dockerdata\plugins:/var/jenkins_home/plugins ^
+        -v c:\Work\dockerdata\plugins:/var/jenkins_home/plugins ^
         jenkins/jenkins:lts-jdk11
 
 docker ps -a | findstr jenkins
