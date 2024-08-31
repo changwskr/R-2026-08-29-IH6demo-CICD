d1fbda8fd657b194c7fdea7b07a4b56212cbe6a6
changwskr <changwskr@naver.com>
2024-08-31 14:55:15 +0900

    virtual

--- CICD/2024-08-31 Virtual Host/2024-08-31-01 [Jenkins] 프로비저닝/01.install/docker run -p 18080 8080 -p 50000 50000 --name jenkins-server.cmd
+++ CICD/2024-08-31 Virtual Host/2024-08-31-01 [Jenkins] 프로비저닝/01.install/docker run -p 18080 8080 -p 50000 50000 --name jenkins-server.cmd
@@ -1,8 +1,7 @@
 rem "container name은 jenkins-server"
-docker run -d -p 18080:8080 -p 50000:50000 ^
-        --name jenkins-server ^
-        --restart=on-failure ^
-        -v c:\Work\dockerdata\plugins:/var/jenkins_home/plugins ^
+docker run -d -p 18080:8080 -p 50000:50000 \
+        --name jenkins-server \
+        --restart=on-failure \
         jenkins/jenkins:lts-jdk11
 
 docker ps -a | findstr jenkins
