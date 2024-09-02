3fab054918dc68e73ec93b417ff545a1e6fb344b
changwskr <changwskr@naver.com>
2024-09-02 23:31:55 +0900

    virtual

--- CICD/2024-08-30 K8S Kubenetes/설치사이트.cmd
+++ CICD/2024-08-30 K8S Kubenetes/설치사이트.cmd
@@ -1,3 +1,7 @@
 https://pyrasis.com/jHLsAlwaysUpToDateKubernetes/Unit06/02
 
 https://github.com/joneconsulting/k8s/blob/master/install/kubernetes_install.md
+
+
+미니쿠버 설치 사이트
+https://idchowto.com/ubuntu-22-04-minikube-%EC%84%A4%EC%B9%98kubectldockerkubernetes/
