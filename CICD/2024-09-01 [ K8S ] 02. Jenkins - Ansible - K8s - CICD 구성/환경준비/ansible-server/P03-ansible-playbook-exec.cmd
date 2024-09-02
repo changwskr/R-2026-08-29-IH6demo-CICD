152a9f309735f93f9a56779e962c7fde2826cfbb
changwskr <changwskr@naver.com>
2024-09-02 17:46:57 +0900

    SK

--- CICD/2024-09-01 [ K8S ] 02. Jenkins - Ansible - K8s - CICD 구성/환경준비/ansible-server/P03-ansible-playbook-exec.cmd
+++ CICD/2024-09-01 [ K8S ] 02. Jenkins - Ansible - K8s - CICD 구성/환경준비/ansible-server/P03-ansible-playbook-exec.cmd
@@ -1,2 +1,3 @@
 ansible-server
-    ansible-playbook -i ./k8s/hosts k8s-cicd-deployment-playbook.yml
+    ansible-playbook -i ./k8s/hosts ./k8s/k8s-cicd-deployment-playbook.yml
+    ansible-playbook -i ./k8s/hosts ./k8s/k8s-cicd-service-playbook.yml
--- CICD/2024-09-01 [ K8S ] 02. Jenkins - Ansible - K8s - CICD 구성/환경준비/ansible-server/P03-ansible-to-k8s-ping.cmd
+++ CICD/2024-09-01 [ K8S ] 02. Jenkins - Ansible - K8s - CICD 구성/환경준비/ansible-server/P03-ansible-to-k8s-ping.cmd
@@ -1,2 +1,11 @@
 ansible-server
     ansible -i ./k8s/hosts k8s -m ping
+
+    [root@a83886fba2b5 ~]# ansible -i ./k8s/hosts k8s -m ping
+    192.168.32.10 | SUCCESS => {
+        "ansible_facts": {
+            "discovered_interpreter_python": "/usr/bin/python3"
+        },
+        "changed": false,
+        "ping": "pong"
+    }
