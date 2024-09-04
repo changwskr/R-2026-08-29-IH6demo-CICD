3e9907493a899b4f2af64b3eb973c21609022ceb
changwskr <changwskr@naver.com>
2024-09-04 22:39:14 +0900

    소나큐브 구성

---
+++ CICD/2024-09-04 [ 소나큐브 ] 01. 정적분석 작업/ST05-빌드.cmd
@@ -0,0 +1,3 @@
+
+cd "C:\Programming(23-08-15)\zRepository\R-2026-08-29-IH6demo-CICD"
+mvn sonar:sonar -Dsonar.host.url=http://192.168.0.6:9000 -Dsonar.login=squ_be03c39b0f496240d167a07fb803790cf7cc644c
